716b1f63adb507531725972d45f92145771045a7
Mateus Nunes <mateusnunes@frwk.com.br>
2023-05-15 13:15:28 -0300

    final

--- src/telas/Inicial.java
+++ src/telas/Inicial.java
@@ -7,7 +7,7 @@ import utils.Utilitarios;
 public class Inicial {
     private static Scanner in = new Scanner(System.in);
 
-    public static void inicial() {
+    public static void run() {
         while (true) {
             System.out.println("\nOlá, bem vindo a nossa Hamburgueria!");
             String pergunta = "Como deseja efetuar login?\n0- Cliente\n1- Funcionario\n2- Encerrar sistema";
